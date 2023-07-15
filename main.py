a73a8b53e10c9f4bf02d64dd0a166470290ba184
Juno <datah07301@gmail.com>
2023-07-15 17:34:56 +0900

    update usage

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
 
 scraper = WebtoonScraper(BASE_PATH)
 scraper.set_driver_options()
-scraper.scrape_links()
 
-links = scraper.get_links()
-for i, link in enumerate(links):
-    print(f"{days[i]} / {len(links[i])}: {link}")
+# scrape the links
+# scraper.scrape_links()
+
+# links = scraper.get_links()
+# for i, link in enumerate(links):
+# print(f"{days[i]} / {len(links[i])}: {link}")
+# print(len(scraper))
+
+# create a rank database
+rank_db = scraper.create_a_rank_database()
+print(rank_db)
+
